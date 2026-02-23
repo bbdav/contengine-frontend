4593c796efd92f4b3565b756dcc652b60a073a68
David Babijaev <dluxstudio@gmail.com>
2026-02-23 13:14:00 -0500

    Fix Articles pagination padding and remove extra bottom padding above

--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -164,7 +164,7 @@ export default function ArticlesPage() {
       }
     >
       {/* Controls + table */}
-      <div className="px-6 pb-6">
+      <div className="px-6 pb-0">
         <div className="mt-4">
           {/* Controls */}
           <div className="flex flex-wrap items-center justify-between gap-3">
@@ -281,7 +281,7 @@ export default function ArticlesPage() {
 
       {/* Sticky pagination bar (right side only) */}
       <div className="sticky bottom-0 z-20 mt-4 border-t border-secondary bg-primary">
-        <div className="py-3">
+        <div className="px-6 py-3">
           <div className="flex items-center justify-between gap-3">
             <Pagination.Root page={page} total={10} onPageChange={setPage}>
               <Pagination.Context>
