a560389e72db9fb2c73417320d2fb4429e9ef243
David Babijaev <dluxstudio@gmail.com>
2026-03-03 20:38:46 -0500

    Add ContentModel page from Figma node 324:10614

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import AppShell from "./layout/AppShell"
 import ArticlesPage from "./pages/ArticlesPage"
+import ContentModelPage from "./pages/ContentModelPage"
+
+// Temporary manual switch until routing is added.
+const CURRENT_PAGE: "articles" | "contentModel" = "contentModel"
 
 export default function App() {
   return (
     <AppShell>
-      <ArticlesPage />
+      {CURRENT_PAGE === "articles" ? <ArticlesPage /> : <ContentModelPage />}
     </AppShell>
   )
 }
