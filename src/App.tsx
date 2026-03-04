ed609538e4746063be3b07b9fe865f5d45f41316
David Babijaev <dluxstudio@gmail.com>
2026-03-03 20:54:51 -0500

    Restore ArticlesPage as default app entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import AppShell from "./layout/AppShell"
 import ArticlesPage from "./pages/ArticlesPage"
-import ContentModelPage from "./pages/ContentModelPage"
-
-// Temporary manual switch until routing is added.
-const CURRENT_PAGE: "articles" | "contentModel" = "contentModel"
 
 export default function App() {
   return (
     <AppShell>
-      {CURRENT_PAGE === "articles" ? <ArticlesPage /> : <ContentModelPage />}
+      <ArticlesPage />
     </AppShell>
   )
 }
